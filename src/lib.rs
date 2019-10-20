bd34e01558bc915beed432a40331831f9e6c4302
naomijub <jnaomi@thoughtworks.com>
2019-10-20 17:46:47 -0300

    Fetching BFF for flights

--- src/lib.rs
+++ src/lib.rs
@@ -24,16 +24,19 @@ pub struct Model {
   fetching: bool,
 
   edit_value: String,
-  value: String,
+  value_bp: String,
+  value_flights: String,
   ft: Option<FetchTask>,
-  payload: Option<BestPrices>,
+  payload_bp: Option<BestPrices>,
+  payload_flights: Option<Flights>
 }
 
 #[derive(Debug)]
 pub enum Msg {
   Update(String),
   Submit,
-  FetchReady(Text),
+  FetchBpReady(Text),
+  FetchFlightsReady(Text),
   Ignore,
   Nope,
 }
@@ -49,16 +52,18 @@ impl Component for Model {
 
       fetching: false,
       edit_value: "".to_string(),
-      value: "https://bff.latam.com/ws/proxy/booking-webapp-bff/v1/public/revenue/bestprices/oneway?departure=2019-11-19&origin=POA&destination=GRU&adult=1&cabin=Y&promoCode=&country=BR&language=pt".to_string(),
+      value_bp: "https://bff.latam.com/ws/proxy/booking-webapp-bff/v1/public/revenue/bestprices/oneway?departure=2019-11-19&origin=POA&destination=GRU&adult=1&cabin=Y&promoCode=&country=BR&language=pt".to_string(),
+      value_flights: "https://bff.latam.com/ws/proxy/booking-webapp-bff/v1/public/revenue/recommendations/oneway?departure=2019-11-20&origin=POA&destination=GRU&adult=1&cabin=Y&country=BR&language=PT&home=pt_br&promoCode=".to_string(),
       link: link,
-      payload: None,
+      payload_bp: None,
+      payload_flights: None,
 
       ft: None,
     }
   }
 
   fn mounted(&mut self) -> ShouldRender {
-        self.fetch_data();
+        self.fetch_flights_data();
 
         true
     }
@@ -70,15 +75,21 @@ impl Component for Model {
             self.edit_value = val;
         }
         Submit => {
-            self.value = self.edit_value.clone();
-            self.fetch_data();
+            self.value_bp = self.edit_value.clone();
+            self.fetch_bp_data();
         }
-        FetchReady(response) => {
-            self.fetching = false;
-            self.payload = Some(
+        FetchBpReady(response) => {
+            self.payload_bp = Some(
               serde_json::from_str(&response.map(|data| data).unwrap()).unwrap()
             );
-        }
+        },
+        FetchFlightsReady(response) => {
+          self.fetching = false;
+          self.payload_flights = Some(
+              serde_json::from_str(&response.map(|data| data).unwrap()).unwrap()
+            );
+          self.fetch_bp_data();
+        },
         Ignore => {
             return false;
         }
@@ -103,6 +114,7 @@ impl Renderable<Model> for Model {
             <div class="header",>{"Lafran"}</div>
             { self.view_data() }
             <section class="container">
+              {self.view_flights()}
             </section>
             <footer class="container" >
               <div class="flight-disclaimer">
@@ -121,12 +133,29 @@ impl Renderable<Model> for Model {
 
 impl Model {
     fn view_data(&self) -> Html<Model> {
-        if let Some(value) = &self.payload {
+        if let Some(value) = &self.payload_bp {
             let mut_value =  value.clone();
             html! {
               <div>
                 { view_itinerary(mut_value.itinerary) }
                 { view_bestprices(mut_value.bestPrices) }
+              </div>
+            }
+        } else {
+            html! {
+              <div/>
+            }
+        }
+    }
+
+    fn view_flights(&self) -> Html<Model> {
+        if let Some(value) = &self.payload_flights {
+            let mut_value =  value.clone();
+            html! {
+              <div class="flight-top">
+                { mut_value.data.flights.into_iter()
+                .map(|flight| view_flight(flight))
+                .collect::<Html<Model>>() }
               </div>
             }
         } else {
@@ -139,7 +168,7 @@ impl Model {
     }
 
   
-    fn fetch_data(&mut self) {
+    fn fetch_bp_data(&mut self) {
         self.fetching = true;
 
         let callback = self.link.send_back(
@@ -147,7 +176,7 @@ impl Model {
                 let (meta, data) = response.into_parts();
 
                 if meta.status.is_success() {
-                    Msg::FetchReady(data)
+                    Msg::FetchBpReady(data)
                 } else {
                     Msg::Ignore
                 }
@@ -156,13 +185,95 @@ impl Model {
 
         let request = Request::builder()
             .method("GET")
-            .uri(self.value.clone())
+            .uri(self.value_bp.clone())
             .body(Nothing)
             .unwrap();
 
         let task = self.fetch_service.fetch(request, callback);
         self.ft = Some(task);
     }
+
+    fn fetch_flights_data(&mut self) {
+      self.fetching = true;
+
+        let callback = self.link.send_back(
+            move |response: Response<Text>| {
+                let (meta, data) = response.into_parts();
+
+                if meta.status.is_success() {
+                    Msg::FetchFlightsReady(data)
+                } else {
+                    Msg::Ignore
+                }
+            },
+        );
+
+        let request = Request::builder()
+            .method("GET")
+            .uri(self.value_flights.clone())
+            .body(Nothing)
+            .unwrap();
+
+        let task = self.fetch_service.fetch(request, callback);
+        self.ft = Some(task);
+    }
+}
+
+pub fn view_flight(flight: Flight) -> Html<Model> {
+  html!{
+    <div class="flight-container">
+      <div class="flight-info">
+        <div class="flight-info-time"> { format!("{} ",flight.departure.time.stamp) } </div>
+        <div class="flight-info-code"> { format!("{} ",flight.departure.airportCode) } </div>
+        <div class="arrow">{ " > " }</div>
+        <div class="flight-info-time"> { format!(" {}",flight.arrival.time.stamp) } </div>
+        <div class="flight-info-code"> { format!(" {}",flight.arrival.airportCode) } </div>
+      </div>
+      <div class="flight-duration">{flight.flightDuration}</div>
+      <div class="flight-price">{flight.cabins[0].displayPrice}</div>
+    </div>
+  }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Flights {
+  pub data: FlightInfo,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct FlightInfo {
+  pub flights: Vec<Flight>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Flight {
+  pub flightCode: String,
+  pub flightDuration: String,
+  pub arrival: FlightInstance,
+  pub departure: FlightInstance,
+  pub cabins: Vec<Cabin>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct FlightInstance {
+  pub airportCode:	String,
+  pub airportName:	String,
+  pub cityCode:	String,
+  pub cityName:	String,
+  pub time: Time,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Time {
+  pub stamp:	String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Cabin {
+  pub displayPrice:	f32,
 }
 
 // <input
